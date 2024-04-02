88c2ae00359182f1fcd05d3becb8848528ebce0d
Karim shamel <karimshamel16@gmail.com>
2024-04-02 04:45:32 +0200

    ClosedApplication class

--- Cwk 4 part 2 templates/ClosedApplication.java
+++ Cwk 4 part 2 templates/ClosedApplication.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class ClosedApplication extends ProcessedApplication{
     // create a constructor to initialize ClosedApplication's objects.
     // and a list of methods to get and retrieve ClosedApplication' information.
@@ -10,5 +12,18 @@ public class ClosedApplication extends ProcessedApplication{
 * */
 
 // define a method to create a closing disclosure
+    String loanID = "123456";
+    Date closingDate = new Date();
+    double closingCost = 5000.0;
 
+    ClosingDisclosure closingDisclosure = new ClosingDisclosure(loanID, closingDate, closingCost);
+
+    public ClosedApplication(String applicationNumber, Borrower borrower, Lender lender, Property property) {
+        super(applicationNumber, borrower, lender, property);
+    }
+
+    public ClosingDisclosure getClosingDisclosure() {
+        return closingDisclosure;
+    }
 }
+
