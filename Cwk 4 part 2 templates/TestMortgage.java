ce7445cede4e7abe4f31b9894861f518f8327322
jasserabdou <jasserabdelfattah12@gmail.com>
2024-04-04 16:31:29 +0200

    all tests added

--- Cwk 4 part 2 templates/TestMortgage.java
+++ Cwk 4 part 2 templates/TestMortgage.java
@@ -68,5 +68,50 @@ class TestMortgage {
         // Print the status of the applications
         System.out.println(processedApplication1.getStatus());
         System.out.println(processedApplication2.getStatus());
+
+        // Create employment and income details for the borrower
+        Employment employment_ = new Employment("Grant", "20-0101-157-8876", "Wall Street", "8 Hours", "Bank Manager",
+                10000);
+        Income income_ = new Income(10000, 200, 300, 100, 0, 2000, 0); // Increase the income
+
+        // Create a bank account and assets for the borrower
+        BankAccount bankAccount_ = new BankAccount("1", "current account", "John", 200000.0, 100000.0, 1.0f, date2);
+        Assets assets_ = new Assets(bankAccount_, 10000, 20000, 50000, 200000, null); // Increase the assets
+
+        // Create debts and credit history for the borrower
+        Debts debts_ = new Debts(100, 200, 300, 400, 500, 600, 700, 800, 900);
+        CreditHistory creditHistory_ = new CreditHistory(0, 0, 0, 0); // No bankruptcies or foreclosures
+
+        // Create the borrower
+        Borrower borrower_ = new Borrower("Emily", "1943-8763-7661", "20-0100-155-9976", date1, employment_, income_,
+                assets_, debts_, creditHistory_);
+
+        // Create two lenders
+        Lender lender1_ = new Lender("1", 2.8f, 9.1, 6f, 2000, 500);
+        Lender lender2_ = new Lender("2", 1.59f, 7.6, 4f, 2000, 500);
+
+        // Create a property
+        Property property_ = new Property("Wall Street", 100000, "ranch-style house", 200, 500, 200);
+
+        // Create two applications, one for each lender
+        Application application1_ = new Application("1", borrower_, lender1_, property_);
+        Application application2_ = new Application("2", borrower_, lender2_, property_);
+
+        // Process the applications
+        ProcessedApplication processedApplication1_ = mortgagemarket.process(application1_);
+        ProcessedApplication processedApplication2_ = mortgagemarket.process(application2_);
+
+        // Compare the loan costs and close the application with the lowest cost
+        if (processedApplication1_.getLoanEstimate() < processedApplication2_.getLoanEstimate()) {
+            mortgagemarket.close(processedApplication1_);
+        } else {
+            mortgagemarket.close(processedApplication2_);
+        }
+
+        // Print the status of the applications
+        System.out.println(processedApplication1_.getStatus());
+        System.out.println(processedApplication2_.getStatus());
+
     }
+
 }
